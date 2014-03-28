21c7f0a43fb7ea7beb09f26f19398fa4d79e6f60
hg <hannahmarizgo@gmail.com>
2014-03-29 00:07:44 +0800

    modified urls and views

--- crime165/crime/views.py
+++ crime165/crime/views.py
@@ -13,28 +13,165 @@ from crime.models import CategoryForm,AgentForm,SuspectForm,LocationForm,CrimeFo
 from django.views.generic.edit import UpdateView
 from django.core.paginator import Paginator, InvalidPage,EmptyPage, PageNotAnInteger
 
+# HOME page
+def index(request, **kwargs):
+	if request.method == 'GET':
+       	  	form = CategoryForm()
+    	else:
+        	form = CategoryForm(request.POST)  
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect('')
+    	return render(request,'crime/index.html', {'form': form})
 
+# CREATE crime, suspect, location, agent, category
 def addCrime(request):
     	if request.method == 'GET':
         	form = CrimeForm()
     	else:
        	        form = CrimeForm(request.POST)
-	         
                 if form.is_valid():
 			form.save()
             		return HttpResponseRedirect('crimelist')
-            
-    	
     	return render(request,'crime/addcrime.html', {'form': form})
+def addSuspect(request):
+    	if request.method == 'GET':
+        	form = SuspectForm()
+    	else:
+       	        form = SuspectForm(request.POST)
+                if form.is_valid():
+			form.save()
+            		return HttpResponseRedirect('suspectlist')
+    	return render(request,'crime/addsuspect.html', {'form': form})
+def addLocation(request):
+    	if request.method == 'GET':
+        	form = LocationForm()
+    	else:
+       	        form = LocationForm(request.POST)
+                if form.is_valid():
+			form.save()
+            		return HttpResponseRedirect('locationlist')
+    	return render(request,'crime/addlocation.html', {'form': form})
+def addAgent(request):
+    	if request.method == 'GET':
+        	form = AgentForm()
+    	else:
+       	        form = AgentForm(request.POST)
+                if form.is_valid():
+			form.save()
+            		return HttpResponseRedirect('addagent')
+    	return render(request,'crime/addagent.html', {'form': form}) 
+def addCategory(request):
+    	if request.method == 'GET':
+        	form = CategoryForm()
+    	else:
+       	        form = CategoryForm(request.POST)
+                if form.is_valid():
+			form.save()
+            		return HttpResponseRedirect('addcategory')
+    	return render(request,'crime/addcategory.html', {'form': form})
 
+# READ crime, suspect, location, agent, category
+def viewCrime(request,id):
+    crime = Crime.objects.get(id=id)    
+    return render(request,'crime/viewcrime.html',{'crime':crime})
+def viewSuspect(request,id):
+    suspect = Suspect.objects.get(id=id)    
+    return render(request,'crime/viewsuspect.html',{'suspect':suspect})
+def viewLocation(request,id):
+    location = Location.objects.get(id=id)    
+    return render(request,'crime/viewlocation.html',{'location':location})
+def viewAgent(request,id):
+    agent = Agent.objects.get(id=id)    
+    return render(request,'crime/viewagent.html',{'agent':agent})
+"""
+def viewCategory(request,id):
+    category = Category.objects.get(id=id)    
+    return render(request,'crime/viewcategory.html',{'category':category})
+"""
+def CrimeList(request):
+    crimelist = Crime.objects.all()
+    paginator = Paginator(crimelist,5)
+    page = request.GET.get('page')
+    try:
+    	crimes = paginator.page(page) 
+    except PageNotAnInteger:
+        crimes = paginator.page(1)
+    except EmptyPage:
+        crimes = paginator.page(paginator.num_pages)
+    return render_to_response('crime/crimelist.html',{'crimes':crimes})
+def SuspectList(request):
+    suspectlist = Suspect.objects.all()
+    paginator = Paginator(suspectlist,5)
+    page = request.GET.get('page')
+    try:
+    	suspects = paginator.page(page) 
+    except PageNotAnInteger:
+        suspects = paginator.page(1)
+    except EmptyPage:
+        suspects = paginator.page(paginator.num_pages)
+    return render_to_response('crime/suspectlist.html',{'suspects':suspects})
+def LocationList(request):
+    locationlist = Location.objects.all()
+    paginator = Paginator(locationlist,5)
+    page = request.GET.get('page')
+    try:
+    	locations = paginator.page(page) 
+    except PageNotAnInteger:
+        locations = paginator.page(1)
+    except EmptyPage:
+        locations = paginator.page(paginator.num_pages)
+    return render_to_response('crime/locationlist.html',{'locations':locations})
+def AgentList(request):
+    agentlist = Agent.objects.all()
+    paginator = Paginator(agentlist,5)
+    page = request.GET.get('page')
+    try:
+    	agents = paginator.page(page) 
+    except PageNotAnInteger:
+        agents = paginator.page(1)
+    except EmptyPage:
+        agents = paginator.page(paginator.num_pages)
+    return render_to_response('crime/agentlist.html',{'agents':agents})
+"""
+def CategoryList(request):
+    categorylist = Category.objects.all()
+    paginator = Paginator(categorylist,5)
+    page = request.GET.get('page')
+    try:
+    	categorys = paginator.page(page) 
+    except PageNotAnInteger:
+        categorys = paginator.page(1)
+    except EmptyPage:
+        categorys = paginator.page(paginator.num_pages)
+    return render_to_response('crime/categorylist.html',{'categorys':categorys})
+"""
+# DELETE crime, suspect, location, agent, category
 def deleteCrime(request,id):
     Crime.objects.get(id=id).delete()
     message = "crime deleted"
     return HttpResponseRedirect('crimelist')
-
+def deleteSuspect(request,id):
+    Suspect.objects.get(id=id).delete()
+    message = "Suspect deleted"
+    return HttpResponseRedirect('suspectlist')
+def deleteLocation(request,id):
+    Location.objects.get(id=id).delete()
+    message = "Location deleted"
+    return HttpResponseRedirect('locationlist')
+def deleteAgent(request,id):
+    Agent.objects.get(id=id).delete()
+    message = "Agent deleted"
+    return HttpResponseRedirect('agentlist')
+"""
+def deleteCategory(request,id):
+    Category.objects.get(id=id).delete()
+    message = "Category deleted"
+    return HttpResponseRedirect('categorylist')
+"""
+# UPDATE crime, suspect, location, agent, category
 def updateCrime(request,id):
     crime = Crime.objects.get(id=id)
-    
     if request.method == 'GET':
         form = CrimeForm(instance = crime)
     else:
@@ -48,35 +185,8 @@ def updateCrime(request,id):
 		crime.save()
         	return HttpResponseRedirect('crimelist')
     return render(request,'crime/updatecrime.html',{'crime':crime,'form':form,'action':'update/'+id})
-
-
-def viewCrime(request,id):
-    crime = Crime.objects.get(id=id)    
-    
-    return render(request,'crime/viewcrime.html',{'crime':crime})
-
-
-def addSuspect(request):
-    	if request.method == 'GET':
-        	form = SuspectForm()
-    	else:
-       	        form = SuspectForm(request.POST)
-	         
-                if form.is_valid():
-			form.save()
-            		return HttpResponseRedirect('suspectlist')
-            
-    	
-    	return render(request,'crime/addsuspect.html', {'form': form})
-
-def deleteSuspect(request,id):
-    Suspect.objects.get(id=id).delete()
-    message = "Suspect deleted"
-    return HttpResponseRedirect('suspectlist')
-
 def updateSuspect(request,id):
     suspect = Suspect.objects.get(id=id)
-    
     if request.method == 'GET':
         form = SuspectForm(instance = suspect)
     else:
@@ -85,40 +195,11 @@ def updateSuspect(request,id):
 		suspect.firstname = request.POST["firstname"]
 		suspect.lastname = request.POST["lastname"]
 		suspect.location_id = request.POST["location"]
-			
 		suspect.save()
         	return HttpResponseRedirect('suspectlist')
     return render(request,'crime/updatesuspect.html',{'suspect':suspect,'form':form,'action':'update/'+id})
-
-
-def viewSuspect(request,id):
-    suspect = Suspect.objects.get(id=id)    
-    
-    return render(request,'crime/viewsuspect.html',{'suspect':suspect})
-
-
-def addLocation(request):
-    	if request.method == 'GET':
-        	form = LocationForm()
-    	else:
-       	        form = LocationForm(request.POST)
-	         
-                if form.is_valid():
-			form.save()
-            		return HttpResponseRedirect('locationlist')
-            
-    	
-    	return render(request,'crime/addlocation.html', {'form': form})
-
-
-def deleteLocation(request,id):
-    Location.objects.get(id=id).delete()
-    message = "Location deleted"
-    return HttpResponseRedirect('locationlist')
-
 def updateLocation(request,id):
     location = Location.objects.get(id=id)
-    
     if request.method == 'GET':
         form = LocationForm(instance = location)
     else:
@@ -130,94 +211,16 @@ def updateLocation(request,id):
 		location.save()
         	return HttpResponseRedirect('locationlist')
     return render(request,'crime/updatelocation.html',{'location':location,'form':form,'action':'update/'+id})
-
-
-def viewLocation(request,id):
-    location = Location.objects.get(id=id)    
-    
-    return render(request,'crime/viewlocation.html',{'location':location})
-
-
-
-def addAgent(request):
-    	if request.method == 'GET':
-        	form = AgentForm()
-    	else:
-       	        form = AgentForm(request.POST)
-	         
-                if form.is_valid():
-			form.save()
-            		return HttpResponseRedirect('addagent')
-            
-    	
-    	return render(request,'crime/addagent.html', {'form': form}) 
-
-def addCategory(request):
-    	if request.method == 'GET':
-        	form = CategoryForm()
-    	else:
-       	        form = CategoryForm(request.POST)
-	         
-                if form.is_valid():
-			form.save()
-            		return HttpResponseRedirect('addcategory')
-            
-    	
-    	return render(request,'crime/addcategory.html', {'form': form})
-
-def index(request, **kwargs):
-	
-	if request.method == 'GET':
-       	  	form = CategoryForm()
-    	else:
-
-        	form = CategoryForm(request.POST)  
-		if form.is_valid():
-			form.save()
-			
-			return HttpResponseRedirect('')
-	
-    	return render(request,'crime/index.html', {'form': form})
-
-def CrimeList(request):
-
-    crimelist = Crime.objects.all()
-    paginator = Paginator(crimelist,5)
-    page = request.GET.get('page')
-    try:
-    	crimes = paginator.page(page) 
-    except PageNotAnInteger:
-        crimes = paginator.page(1)
-    except EmptyPage:
-        crimes = paginator.page(paginator.num_pages)
-
-    return render_to_response('crime/crimelist.html',{'crimes':crimes})
-
-def SuspectList(request):
-
-    suspectlist = Suspect.objects.all()
-    paginator = Paginator(suspectlist,5)
-    page = request.GET.get('page')
-    try:
-    	suspects = paginator.page(page) 
-    except PageNotAnInteger:
-        suspects = paginator.page(1)
-    except EmptyPage:
-        suspects = paginator.page(paginator.num_pages)
-
-    return render_to_response('crime/suspectlist.html',{'suspects':suspects})
-
-def LocationList(request):
-
-    locationlist = Location.objects.all()
-    paginator = Paginator(locationlist,5)
-    page = request.GET.get('page')
-    try:
-    	locations = paginator.page(page) 
-    except PageNotAnInteger:
-        locations = paginator.page(1)
-    except EmptyPage:
-        locations = paginator.page(paginator.num_pages)
-
-    return render_to_response('crime/locationlist.html',{'locations':locations})
-
+def updateAgent(request,id):
+    agent = Agent.objects.get(id=id)
+    if request.method == 'GET':
+        form = AgentForm(instance = location)
+    else:
+        form = AgentForm(request.POST)         
+    	if form.is_valid():
+		agent.firstname = request.POST["firstname"]
+		agent.lastname = request.POST["lastname"]
+		agent.location_id = request.POST["location"]
+		agent.save()
+        	return HttpResponseRedirect('agentlist')
+    return render(request,'crime/updateagent.html',{'agent':agent,'form':form,'action':'update/'+id})
